de6e27eb310f15dac7a2a7cbb2593357febe3baf
DeepFlow-pengbin <84362246+1473371932@users.noreply.github.com>
2024-08-27 10:07:38 +0800

    更改 python3 执行位置

--- df-llm-agent/py2c.sh
+++ df-llm-agent/py2c.sh
@@ -6,7 +6,7 @@ echo "Starting py2c.sh script"
 
 cp llm_agent_app/llm_agent.py llm_agent_app/llm_agent.pyx
 
-/root/venv/bin/python3 setup.py build_ext  -i
+/venv/bin/python3 setup.py build_ext  -i
 
 rm llm_agent_app/llm_agent.py
 rm llm_agent_app/llm_agent.c
